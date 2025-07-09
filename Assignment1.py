2a67688a175d7cedb80e05eb82e8f056b02dd981
lavxxshz <palluvkush355@gmail.com>
2025-07-09 18:36:42 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+#Task 1: Perform Basic Mathematical Operations
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+#Addition
+print("Addition is ",a+b)
+print("Subtraction is ",a-b)
+print("Multiplication" ,a*b)
+print("Division" ,a/b)
+
+
+name=input("Enter your first name name")
+name1=input("Enter your last name name")
+
+print("welcome",name ,name1,"I hope your day go well")
